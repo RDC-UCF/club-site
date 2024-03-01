8085526f93cd20c0ae3cb8a7f374e914025e2d8a
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-02-29 23:11:16 -0500

    remove default style tag for App

--- app/src/App.vue
+++ app/src/App.vue
@@ -7,7 +7,7 @@
   <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
-<style scoped>
+<!-- <style scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -20,4 +20,4 @@
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
-</style>
+</style> -->
