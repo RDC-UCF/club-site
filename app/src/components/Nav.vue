1a54eb42f754bea55b806f470aace0aeea042a1c
Pharit Smitasin <pharitsmitasin@gmail.com>
2024-03-03 13:16:15 -0500

    define html content for nav, to be styled

--- app/src/components/Nav.vue
+++ app/src/components/Nav.vue
@@ -3,5 +3,19 @@ import "../style.css";
 </script>
 
 <template>
-  <p class="underline">Roblox Development Club at UCF</p>
+  <nav
+    class="flex flex-row items-center text-start bg-main-text-color md:block pt-10"
+  >
+    <a>RDC @ UCF</a>
+
+    <a>About</a>
+    <a>Our Team</a>
+    <a>Calendar</a>
+    <a>Contact Us</a>
+  </nav>
+  <!-- <p
+    class="flex-1 flex-row relative hidden items-center text-end bg-main-text-color md:block"
+  >
+    RDC @ UCF
+  </p> -->
 </template>
