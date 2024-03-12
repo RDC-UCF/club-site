bf8081e569a9478407982251b6c127eb70de9b70
Matthew Labrada <matthew.labrada@outlook.com>
2024-03-12 14:02:25 -0400

    Updated image paths and discord inv link

--- app/src/components/Nav.vue
+++ app/src/components/Nav.vue
@@ -3,7 +3,7 @@
     <nav :class="{ 'translate-y-0': isShown, '-translate-y-full': !isShown, 'opacity-100': isShown, 'opacity-0': !isShown }" class="transform transition-opacity duration-300 w-full">
       <div class="flex justify-between items-center w-full p-4">
         <a href="/" class="flex items-center">
-          <img src="/public/logo1.png" alt="RDC @ UCF" class="h-8">
+          <img src="/logo1.png" alt="RDC @ UCF" class="h-8">
           <span class="flex items-center space-x-4 justify-end">RDC @ UCF</span>
         </a>
         <div class="flex items-center space-x-4 justify-end">
@@ -11,8 +11,8 @@
           <a href="/our-team" class="hover:text-gray-700">Our Team</a>
           <a href="/calendar" class="hover:text-gray-700">Calendar</a>
           <a href="/contact" class="hover:text-gray-700">Contact Us</a>
-          <a href="your-discord-invite-link" target="_blank" rel="noopener noreferrer">
-            <img src="/public/discord_logo.png" alt="Discord" class="h-6">
+          <a href="https://discord.gg/ASngpDyJGg" target="_blank" rel="noopener noreferrer">
+            <img src="/discord_logo.png" alt="Discord" class="h-6">
           </a>
         </div>
       </div>
