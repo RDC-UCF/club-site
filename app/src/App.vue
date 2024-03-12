c36c5b499c77e8505f2280c04783af7fb0d2d4b3
Matthew Labrada <matthew.labrada@outlook.com>
2024-03-12 14:31:30 -0400

    added text/fixed positioning on homepage

--- app/src/App.vue
+++ app/src/App.vue
@@ -10,11 +10,20 @@ import AboutTwo from "./components/AboutTwo.vue";
 <template>
   <div class="min-h-screen flex flex-col bg-gradient-to-t from-[#B5A26A] to-[#F8FAFC]">
     <Nav />
-    <div class="flex-1 flex-row relative hidden bg-main-text-color md:block">
-      <KnightOne />
+    <div class="flex-1 flex flex-row items-center justify-center p-8 text-center md:text-left">
+      <div class="flex-1">
+        <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold my-4 text-gray-800">
+          Roblox<br> Development<br> Club at UCF.
+        </h1>
+      </div>
+      <div class="flex-1">
+        <KnightOne/>
+      </div>
+    </div>
+    <div class="flex-1 flex flex-row relative hidden md:flex">
       <KnightTwo />
     </div>
-    <div class="flex-1 flex-row relative hidden bg-main-text-color md:block">
+    <div class="flex-1 flex flex-row relative hidden md:flex">
       <AboutOne />
       <AboutTwo />
     </div>
