0ad0ef05ede9d695462ca7963df804f46bda42fc
Matthew Labrada <matthew.labrada@outlook.com>
2024-03-12 12:15:02 -0400

    Changes to be committed: 	modified:   app/src/App.vue  Cleaned up App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-// import HelloWorld from "./components/HelloWorld.vue";
 import Nav from "./components/Nav.vue";
 import "./style.css";
 import KnightOne from "./components/KnightOne.vue";
@@ -9,7 +8,7 @@ import AboutTwo from "./components/AboutTwo.vue";
 </script>
 
 <template>
-  <!-- <div class="mx-[5%] md:mx-[5%] lg:mx-[5%] min-h-screen flex flex-col"> -->
+
   <div class="mx-[5%] min-h-screen flex flex-col">
     <Nav />
     <Title />
@@ -22,20 +21,4 @@ import AboutTwo from "./components/AboutTwo.vue";
       <AboutTwo />
     </div>
   </div>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
-
-<!-- <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style> -->
