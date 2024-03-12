8136c5a51512a0dd2ed11a8bdbf9b3b4d8f26a0c
Matthew Labrada <matthew.labrada@outlook.com>
2024-03-12 15:17:22 -0400

    smooth resizing, navbar padding in style.css

--- app/src/App.vue
+++ app/src/App.vue
@@ -27,11 +27,26 @@ import AboutTwo from "./components/AboutTwo.vue";
     <div class="flex-1 px-4 md:px-8 lg:px-16 xl:px-32">
     <div class="flex-1 px-4 md:px-8 lg:px-16 xl:px-32" style="padding-top: var(--navbar-height);">
       <div class="flex flex-col md:flex-row items-center justify-between text-center md:text-left h-full">
-        <h1 class="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold my-4 text-gray-800 flex-1">
+        <h1 class="title text-4xl md:text-6xl lg:text-8xl xl:text-10xl font-bold my-4 text-gray-800 flex-1">
           Roblox<br> Development<br> Club at UCF.
         </h1>
         <div class="flex-1">
-          <KnightOne class="w-full h-auto"/>
+          <KnightOne class="logo w-full h-auto"/>
+        </div>
+      </div>
+    </div>
+    <div class="flex-1 hidden md:flex">
+    <div class="flex-1 flex flex-row items-center justify-center p-8 text-center md:text-left">
+      <div class="flex-1">
+        <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold my-4 text-gray-800">
+    <div class="flex-1 px-4 md:px-8 lg:px-16 xl:px-32">
+    <div class="flex-1 px-4 md:px-8 lg:px-16 xl:px-32" style="padding-top: var(--navbar-height);">
+      <div class="flex flex-col md:flex-row items-center justify-between text-center md:text-left h-full">
+        <h1 class="title text-4xl md:text-6xl lg:text-8xl xl:text-10xl font-bold my-4 text-gray-800 flex-1">
+          Roblox<br> Development<br> Club at UCF.
+        </h1>
+        <div class="flex-1">
+          <KnightOne class="logo w-full h-auto"/>
         </div>
       </div>
     </div>
@@ -56,8 +71,23 @@ import AboutTwo from "./components/AboutTwo.vue";
 </style>
 
 
-<style>
-:root {
-  --navbar-height: 64px;
+<style scoped>
+.title {
+  transition: font-size 0.3s ease-in-out;
+}
+
+.logo {
+  transition: width 0.3s ease-in-out, height 0.3s ease-in-out;
+}
+</style>
+
+
+<style scoped>
+.title {
+  transition: font-size 0.3s ease-in-out;
+}
+
+.logo {
+  transition: width 0.3s ease-in-out, height 0.3s ease-in-out;
 }
 </style>
