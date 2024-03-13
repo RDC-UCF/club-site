39e9ef20f84c4ddf08e211fc766781e80583c6c0
Matthew Labrada <matthew.labrada@outlook.com>
2024-03-13 06:35:14 -0400

    more resizing

--- app/src/App.vue
+++ app/src/App.vue
@@ -8,22 +8,18 @@ import AboutTwo from "./components/AboutTwo.vue";
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col bg-gradient-to-t from-[#B5A26A] to-[#F8FAFC]">
+  <div class="flex flex-col bg-gradient-to-t from-[#B5A26A] to-[#F8FAFC]">
     <Nav />
-    <div class="flex-1 px-4 md:px-8 lg:px-16 xl:px-32" style="padding-top: var(--navbar-height);">
-      <div class="flex flex-col md:flex-row items-center justify-between text-center md:text-left h-full">
-        <h1 class="title text-4xl md:text-6xl lg:text-8xl xl:text-10xl font-bold my-4 text-gray-800 flex-1">
-          Roblox<br> Development<br> Club at UCF.
-        </h1>
-        <div class="flex-1">
-          <KnightOne class="logo w-full h-auto"/>
-        </div>
-      </div>
+    <div class="flex-1 flex flex-col md:flex-row items-center justify-center text-center md:text-left pt-[var(--navbar-height)] px-[2rem] md:px-[4rem] lg:px-[6rem] xl:px-[8rem]" style="min-height: calc(100vh - var(--navbar-height));">
+      <h1 class="title text-4xl md:text-6xl lg:text-8xl xl:text-16xl font-bold my-4 text-gray-800">
+        Roblox<br> Development<br> Club at UCF.
+      </h1>
+      <KnightOne class="logo w-[80vw] h-[80vh] md:w-[55vw] md:h-[55vh] lg:w-[80vw] lg:h-[80vh] object-contain mx-auto"/>
     </div>
-    <div class="flex-1 hidden md:flex">
+    <div class="section min-h-screen">
       <KnightTwo />
     </div>
-    <div class="flex-1 hidden md:flex">
+    <div class="section min-h-screen">
       <AboutOne />
       <AboutTwo />
     </div>
@@ -38,4 +34,7 @@ import AboutTwo from "./components/AboutTwo.vue";
 .logo {
   transition: width 0.3s ease-in-out, height 0.3s ease-in-out;
 }
+
+.section {
+  min-height: 100vh;}
 </style>
